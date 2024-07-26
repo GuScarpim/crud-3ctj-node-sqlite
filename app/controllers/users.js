bbb0c243de67627f2763a60e06dc12207d722208
guscarpim <gustavoscarpim@gmail.com>
2024-07-25 21:29:27 -0300

    update project

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -14,12 +14,37 @@ const getUsers = (req, response) => {
   })
 }
 
-const createUsers = (req, response) => {
+const getUsersById = (req, response) => {
+  /**
+   * Façam a busca por id utilizando o mesmo padrão das outras controllers
+   */
+}
+
+const createUsers = (req, res) => {
   const { id = this.lastID, name, lastName, age, email } = req.body;
 
-  const sql = "INSERT INTO users (id, name, lastName, age, email) VALUES (?, ?, ?, ?, ?)"
+  const sql = "INSERT INTO users (id, name, lastName, age, email) VALUES (?, ?, ?, ?, ?)";
 
   db.run(sql, [id, name, lastName, age, email], (err) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: { id, name, lastName, age, email }
+    });
+  });
+}
+
+const updateUsers = (req, response) => {
+  const { id } = req.params;
+  const { name, lastName, age, email } = req.body;
+
+  const sql = "UPDATE users SET name = ?, lastName = ?, age = ?, email = ? WHERE id = ?";
+
+  db.run(sql, [name, lastName, age, email, id], (err) => {
     if (err) {
       response.status(500).json({ error: err.message });
       return;
@@ -27,12 +52,33 @@ const createUsers = (req, response) => {
 
     response.json({
       message: 'success',
-      data: { id, name, lastName, age, email }
-    })
-  })
+      data: { id, name, lastName, age, email },
+      changes: this.changes
+    });
+  });
+}
+
+const deleteUsers = (req, response) => {
+  const { id } = req.params;
+
+  const sql = "DELETE FROM users WHERE id = ?";
+
+  db.run(sql, id, (err) => {
+    if (err) {
+      response.status(500).json({ error: err.message });
+      return;
+    }
+
+    response.json({
+      message: 'Deletado!',
+      changes: this.changes
+    });
+  });
 }
 
 module.exports = {
   getUsers,
-  createUsers
+  createUsers,
+  updateUsers,
+  deleteUsers
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 
 router.get('/users', usersController.getUsers);
-router.get('/users', usersController.createUsers);
+router.post('/users', usersController.createUsers);
+router.put('/users/:id', usersController.updateUsers);
+router.delete('/users/:id', usersController.deleteUsers);
 
 module.exports = router;
