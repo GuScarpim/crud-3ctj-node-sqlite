ce98007ae9701f692bf2c909eff8fb5b9a1897ae
guscarpim <gustavoscarpim@gmail.com>
2024-09-04 22:45:38 -0300

    update project

--- app/index.js
+++ app/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const productsRoutes = require('./routes/products');
 
+// Habilitar CORS para localhost:3000
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
 
-app.use('/', productsRoutes)
+app.use('/', productsRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
