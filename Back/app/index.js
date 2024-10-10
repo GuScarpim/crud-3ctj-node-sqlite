7434997c1d171cfe9980c2ce897b97659b3127a9
guscarpim <gustavoscarpim@gmail.com>
2024-10-10 20:03:00 -0300

    update project

--- Back/app/index.js
+++ Back/app/index.js
@@ -6,7 +6,7 @@ const productsRoutes = require('./routes/products');
 
 // Habilitar CORS para localhost:5173
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: ['http://localhost:5173', 'https://crud-3ctj-node-sqlite-1.onrender.com/products'],
 }));
 
 app.use(express.json());
